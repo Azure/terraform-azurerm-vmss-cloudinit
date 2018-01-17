90f8b4ad412ffff6c422b544abe8e69835d76aa3
David Tesar <david.tesar@microsoft.com>
2018-01-17 07:06:29 -0800

    Fix formatting

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -3,38 +3,39 @@ provider "azurerm" {
 }
 
 variable "resource_group_name" {
-    default = "terraform-vmss-cloudinit"
+  default = "terraform-vmss-cloudinit"
 }
 
 module "network" {
-    source = "Azure/network/azurerm"
-    location = "westus"
-    allow_ssh_traffic = "true"
-    resource_group_name = "${var.resource_group_name}"
+  source              = "Azure/network/azurerm"
+  location            = "westus"
+  allow_ssh_traffic   = "true"
+  resource_group_name = "${var.resource_group_name}"
 }
 
 module "loadbalancer" {
-  source = "Azure/loadbalancer/azurerm"
+  source              = "Azure/loadbalancer/azurerm"
   resource_group_name = "${var.resource_group_name}"
-  location = "westus"
-  prefix = "terraform-test"
+  location            = "westus"
+  prefix              = "terraform-test"
+
   "lb_port" {
-      http = [ "80", "Tcp", "80"]
-      ssh = ["22", "Tcp", "22"]
+    http = ["80", "Tcp", "80"]
+    ssh  = ["22", "Tcp", "22"]
   }
 }
 
 module "computegroup" {
-    source              = ".."
-    resource_group_name = "${var.resource_group_name}"
-    cloudconfig_file    = "${path.module}/cloudconfig.tpl"
-    location            = "westus"
-    vm_size             = "Standard_DS2_v2"
-    admin_username      = "azureuser"
-    admin_password      = "ComplexPassword"
-    ssh_key             = "~/.ssh/id_rsa.pub"
-    nb_instance         = 1
-    vm_os_simple        = "UbuntuServer"
-    vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-    load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
+  source                                 = ".."
+  resource_group_name                    = "${var.resource_group_name}"
+  cloudconfig_file                       = "${path.module}/cloudconfig.tpl"
+  location                               = "westus"
+  vm_size                                = "Standard_DS2_v2"
+  admin_username                         = "azureuser"
+  admin_password                         = "ComplexPassword"
+  ssh_key                                = "~/.ssh/id_rsa.pub"
+  nb_instance                            = 1
+  vm_os_simple                           = "UbuntuServer"
+  vnet_subnet_id                         = "${module.network.vnet_subnets[0]}"
+  load_balancer_backend_address_pool_ids = "${module.loadbalancer.azurerm_lb_backend_address_pool_id}"
 }
